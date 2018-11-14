aab6954f8de2f2bd0a73feefc946e08185ab296c
bishalshah991 <bishalshah991@gmail.com>
2018-11-14 11:11:36 +0545

    assignment

---
+++ Assignment/ExcelReader.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace Assignment
+{
+    public class ExcelReader
+    {
+        public IWebDriver driver;
+        public string ExcelDataReader(int x, int y)
+        {
+
+            excel.Application Xapp = new excel.Application();
+            excel.Workbook Xworkbook = Xapp.Workbooks.Open(@"C:\Users\BShah\Desktop\Office_Document\SeleniumScript\ExcelReader\Assignment\Assignment\TestData.xlsx");
+            excel._Worksheet Xworksheet = Xworkbook.Sheets[1];
+            excel.Range Xrange = Xworksheet.UsedRange;
+            return Xrange.Cells[x][y].value2;
+        }
+
+    }
+}
---
+++ Assignment/TestMethod1.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    [TestFixture]
+    public class TestMethod1: ExcelReader
+    {
+        ExcelReader x = new ExcelReader();
+        string ExpecteTitle = "Yahoo";
+
+   
+        [SetUp]
+        public void BrowserSetup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http:www.google.com");
+            //System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Browser has opened");
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+
+                driver.FindElement(By.XPath("//input[@id='lst-ib']")).SendKeys(x.ExcelDataReader(i, 1));
+                driver.FindElement(By.XPath("//input[@id='lst-ib']")).SendKeys(Keys.Enter);
+                
+
+                if(x.ExcelDataReader(i, 1).Equals("Yahoo"))
+                {
+
+                    driver.FindElement(By.XPath("//cite[contains(text(),'https://sg.yahoo.com/')]")).Click();
+                    String Ytitle = driver.Title;
+                    Console.WriteLine("The Title of page:-"+Ytitle);
+                    Console.WriteLine("Test Is Passed");
+                    driver.Navigate().Back();
+                    driver.FindElement(By.XPath("//input[@id='lst-ib']")).Clear();
+                    System.Threading.Thread.Sleep(5000);
+                    continue;
+                }
+                
+                
+                driver.FindElement(By.XPath("//input[@id='lst-ib']")).Clear();
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
